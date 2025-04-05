4a1b143396db83ae4bdec4b94c53ca2eedb0c31b
AthenaWrenn <162925464+AthenaWrenn@users.noreply.github.com>
2025-04-05 01:57:22 -0400

    scope limits

--- tfid.py
+++ tfid.py
@@ -9,16 +9,21 @@ os.makedirs('visualizations', exist_ok=True)
 file_path = "winemag-data-130k-v2.csv"
 df = pd.read_csv(file_path)
 
-#10 desciptions
-df = df[['description']].dropna().head(10)
+#desciptions
+#df = df[['description']].dropna().head(10)
+df = df[['description']].dropna()
 
 #features; exclude the unwanted words
-custom_stopwords = list(ENGLISH_STOP_WORDS.union(['2012', '2016', 'alongside', 'aromas', 'certainly', 'include', 'notes', 'offering', 'offers', 'palate']))
+custom_stopwords = list(ENGLISH_STOP_WORDS.union(['alongside', 'aromas', 'certainly', 'include', 'notes', 'offering', 'offers', 'palate', 'does', 'doesn', 'feature', 'features', 'flavor',
+                                                   'flavored', 'flavors', 'gets', 'given', 'gives', 'giving', 'goes', 'going', 'hint', 'hints', 'holds', 'delivers', 'drink', 'drinking', 'feel',
+                                                    'feels', 'followed', 'like', 'make', 'tastes', 'tones']))
 
-vectorizer = TfidfVectorizer(stop_words=custom_stopwords)
+#no excludes, no numbers, nothing in more than 55% of stuff, nothing that shows up less than 1000 times
+vectorizer = TfidfVectorizer(stop_words=custom_stopwords, token_pattern=r'\b[a-zA-Z]{2,}\b', max_df=0.55, min_df=1000)
 X = vectorizer.fit_transform(df['description'])
 
 #Excel File Please
+print("sheet?")
 tfidf_df = pd.DataFrame(X.toarray(), columns=vectorizer.get_feature_names_out())
 tfidf_df.to_excel('visualizations/tfidf_matrix.xlsx', index=False)
 print("Saved TF-IDF matrix to visualizations/tfidf_matrix.xlsx")
